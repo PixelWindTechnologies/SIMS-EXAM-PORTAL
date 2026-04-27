ae0b458453b909aeaa83601fc4f9131ea9c113d6
Tejaswini Reddy <tejaswinireddy8905@gmail.com>
2026-04-27 23:31:50 +0530

    Fix API base URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_BASE = process.env.REACT_APP_API_URL;
 
-const api = axios.create({ baseURL: API_BASE });
+const api = axios.create({ baseURL: `${API_BASE}/api` });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
